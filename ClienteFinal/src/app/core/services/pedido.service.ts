7223428aa17154b030fe203d51fe35f858295b5c
Martiniano-Olivera <42952279+Martiniano-Olivera@users.noreply.github.com>
2025-09-07 19:33:27 -0300

    fix file upload to send pdf files

--- ClienteFinal/src/app/core/services/pedido.service.ts
+++ ClienteFinal/src/app/core/services/pedido.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Archivo, OpcionesImpresion, Pedido } from '../models/pedido.model';
 
 @Injectable({
