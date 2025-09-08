c72fed99ea1143088df90d76da252e9a22d12287
Martiniano-Olivera <42952279+Martiniano-Olivera@users.noreply.github.com>
2025-09-07 21:23:43 -0300

    chore: add missing nuevo pedido style

--- ClienteFinal/src/app/core/services/orders-public.service.ts
+++ ClienteFinal/src/app/core/services/orders-public.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 export interface PublicOrder {
@@ -18,9 +18,7 @@ export class OrdersPublicService {
     const fd = new FormData();
     fd.set('clienteNombre', input.nombre);
     fd.set('clienteTelefono', input.telefono);
-    for (const f of input.files) fd.append('files', f, f.name);
-    return this.http
-      .post<PublicOrder>(`${environment.apiUrl}/public/orders`, fd, { observe: 'response' })
-      .pipe(map(r => r.body as PublicOrder));
+    input.files.forEach(f => fd.append('files', f, f.name));
+    return this.http.post<PublicOrder>(`${environment.apiUrl}/public/orders`, fd);
   }
 }
